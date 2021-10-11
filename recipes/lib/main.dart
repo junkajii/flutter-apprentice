4e21acebb1bdb2533328f9b0dc7876bb1299fa61
Jun Kajii <junk4jii@gmail.com>
2021-10-11 19:36:17 -0300

    🔥 remove comments

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -8,23 +8,17 @@ class RecipeApp extends StatelessWidget {
   const RecipeApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
-  // 1
   @override
   Widget build(BuildContext context) {
-    // 2
     final ThemeData theme = ThemeData();
-    // 3
     return MaterialApp(
-      // 4
       title: 'Recipe Calculator',
-      // 5
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
           primary: Colors.grey,
           secondary: Colors.black,
         ),
       ),
-      // 6
       home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
