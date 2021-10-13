7f54e3f6468ec575db009a24d68f98f59f99f46b
Jun Kajii <junk4jii@gmail.com>
2021-10-13 18:07:37 -0300

    ✨ add recipe detail

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -46,7 +47,20 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
-            // TODO: Add GestureDetector
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+
             return buildRecipeCard(Recipe.samples[index]);
           },
         ),
